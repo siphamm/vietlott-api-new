5f61ea3befcb8420241786ff3f7965ca07ed34da
Si Pham <phamtrisi@gmail.com>
2023-10-02 00:51:41 +0900

    added cors

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 import csv
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class AddResultInput(BaseModel):
